df2129a2c35663a8d6a73df509fc154b37fa198d
Camila Aballay <aballaycamila4@gmail.com>
2025-01-04 17:54:46 -0300

    se modificó ejemplar.php

---
+++ model/entities/Ejemplar.php
@@ -0,0 +1,38 @@
+<?php
+namespace model\entities;
+
+
+final class Ejemplar{
+private $idLibro,$codigo,$observacion, $id;
+    function __construct($idLibro){
+        $this->idLibro= $idLibro;
+        $this->id=0;
+        $this->codigo=0;
+        $this->observacion="";
+  
+      }
+    
+    function getIdLibro():int{
+        return $this->idLibro;
+    }
+    function getId():int{
+        return $this->id;
+    }
+    function getCodigo():int{
+        return $this->codigo;
+    }
+    function getObs():string{
+        return $this->observacion;
+    }
+    
+    public function setIdLibro($idLibro):void{
+        $this->idLibro = (is_integer($idLibro) && ($idLibro > 0)) ? $idLibro : 0;        
+    }
+    public function setCodigo($codigo){
+        $this->codigo= (is_integer($codigo) && ($codigo >=1 && $codigo<=9999)) ? $codigo: 0;
+    }
+    public function setObs($obs): void{
+
+        $this->observacion= ((is_string($obs))&&(strlen(trim($obs)) <= 45)) ? trim($obs) : "";
+    }
+}
