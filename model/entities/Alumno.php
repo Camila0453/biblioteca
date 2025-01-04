c84f5b55c5ea7c08c415dae240e94c0621ae2d2b
Camila Aballay <aballaycamila4@gmail.com>
2025-01-04 03:19:05 -0300

    se modificó alumno.php

--- model/entities/Alumno.php
+++ model/entities/Alumno.php
@@ -3,12 +3,41 @@ namespace model\entities;
 
 
 final class Alumno{
- private $idSocio,$id,$carreras;
+ private $idSocio,$id,  $carreras;
 
 
  function __construct($idSocio){
-
+    $this->idSocio= $idSocio;
+    $this->id=0;
+    $this->carreras=[];
 
  }
+
+ function getIdSocio():int{
+    return $this->idSocio;
+}
+function getId():int{
+    return $this->id;
+}
+function getCarreras():array{
+    return $this->carreras;
+}
+
+public function setSocio($idSocio):void{
+    $this->idSocio = (is_integer($idSocio) && ($idSocio > 0)) ? $idSocio : 0;        
+}
+function aggCarreras($materia){
+    array_push($this->carreras,$materia);
+}
+
+public function toJson(): object{
+    $json = json_decode('{}');
+    $json->{"id"} = $this->getId();
+    $json->{"idSocio"} = $this->getIdSocio();
+    $json->{"carreras"} = $this->getCarreras();
+    
+    return $json;        
+}
+
 
 }
